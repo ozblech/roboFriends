7fb6be6a3febee0aa8014521286d60222723679a
ozblech <ozblech87@gmail.com>
2020-04-02 15:50:10 +0300

    added ErrorBoundry

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class ErrorBoundry extends Component {
+	constructor() {
+		super();
+		this.state = {
+			hasError: false
+		}
+	}
+
+	componentDidCatch() {
+		this.setState({ hasError: true });
+	}
+
+	render() {
+		if(this.state.hasError) {
+			return <h1>"Oooops. That is not good!"</h1>
+		}
+		else {
+			return this.props.children;
+		}
+	}
+
+}
+
+
+
+
+export default ErrorBoundry;
